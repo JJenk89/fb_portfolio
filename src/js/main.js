86bf98d396855a77e9fe38f92711c173e14fa12b
jjenk <j.jenkinson89@hotmail.com>
2024-10-10 15:54:56 +0200

    added js menu, added imgs to flowers gallery, reworked some designs

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,30 @@
-//Init app
 //Lightboxes
 //Carousel
+
 //Burger
 const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
 const cross = document.getElementById("cross");
+const menuLinks = document.querySelectorAll("a");
 
 burger.addEventListener("click", () => {
-	console.log("burger clicked");
 	menu.classList.remove("hidden");
 	cross.classList.remove("hidden");
 	burger.classList.toggle("hidden");
+	document.body.classList.add("disable-scroll");
 });
 
 cross.addEventListener("click", () => {
 	cross.classList.toggle("hidden");
 	menu.classList.toggle("hidden");
 	burger.classList.toggle("hidden");
+	document.body.classList.remove("disable-scroll");
+});
+
+//removes menu on link click inside the menu
+menuLinks.forEach((a) => {
+	a.addEventListener("click", () => {
+		document.body.classList.remove("disable-scroll");
+		menu.classList.remove("hidden");
+	});
 });
